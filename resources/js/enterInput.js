88ca9c7f6b3b0eaddf41c35420b27ab5450bc234
joch3em <57416031+joch2em@users.noreply.github.com>
2024-12-17 20:03:26 +0100

    js

--- resources/js/enterInput.js
+++ resources/js/enterInput.js
@@ -2,12 +2,16 @@ const EIinput = document.getElementById('EIinput');
 const EIconfirm = document.getElementById('EIconfirm');
 const EIusername = document.getElementById('EIusername');
 
-EIconfirm.addEventListener('click', () => {
-    if(EIinput.value !== '' && EIinput.value.length == 1 && EIinput.value !== ' ' && isNaN(EIinput.value)){ 
-        EIusername.innerHTML += EIinput.value;
-        EIinput.value = '';
-    }
-    else{
-        alert('Please enter a valid character');
-    }
-});
+if (EIconfirm) {
+    EIconfirm.addEventListener('click', () => {
+        if(EIinput.value !== '' && EIinput.value.length == 1 && EIinput.value !== ' ' && isNaN(EIinput.value)){ 
+            EIusername.innerHTML += EIinput.value;
+            EIinput.value = '';
+        }
+        else{
+            alert('Please enter a valid character');
+        }
+    });
+} else {
+    console.error('EIconfirm element not found');
+}
